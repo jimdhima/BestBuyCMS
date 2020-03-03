7b08329ac5f4c1f59a061b17257cdd375c6bd636
jimdhima <xdhima@gmail.com>
2020-03-02 21:07:56 -0500

    search

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from 'axios';
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
-  const foo = e => {
-    axios.get('api/products?q=iphone').then(console.log).catch(console.log)
-  }
-
   return (
-    <div>
-      <button type="button" onClick={(e) => foo(e)}>Test</button>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/cart" component={Cart} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import List from '../components/List';
+
+export default function Home() {
+    return ( 
+        <div>
+            <List />
+        </div>
+    )
+}
